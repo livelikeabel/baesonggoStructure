881c11fb0024eea0ab23c68a4037522e73f78968
seungkyu2 <zx6658@naver.com>
2018-03-13 10:47:50 +0900

    add detail env and edit api code

--- src/api/vroong.js
+++ src/api/vroong.js
@@ -7,6 +7,15 @@ const apiKey = process.env.VROONG_API_KEY ;
 const secretKey = process.env.VROONG_SECRET_KEY;
 const path = '/api/delivery/track';
 
+//부릉 API는 HMAC-SHA256 을 이용한 커스텀 인증 헤더값을 만들어 넣은 뒤 호출해야만 합니다.
+//따라서 아래와 같은 작업을 수행합니다.
+function generateHMACDigest(delivery_id) {
+  const method = 'POST';
+  return crypto.createHmac('sha256', secretKey)
+    .update(`${method}${path}${delivery_id}`)
+    .digest('hex');
+}
+
 //배송중 조회 데이터 받아오기
 export function getDelivery(delivery_id) {
 
@@ -29,12 +38,4 @@ const Hmac = `${apiKey}:${hmacDigest}`;
       }).catch(({ response }) => {
         throw response.data.error;
       });
-}
-//부릉 API는 HMAC-SHA256 을 이용한 커스텀 인증 헤더값을 만들어 넣은 뒤 호출해야만 합니다. 
-//따라서 아래와 같은 작업을 수행합니다.
-function generateHMACDigest(delivery_id) {
-  const method = 'POST';
-  return crypto.createHmac('sha256', secretKey)
-    .update(`${method}${path}${delivery_id}`)
-    .digest('hex');
 }
--- src/utils/vroongRequest.js
+++ src/utils/vroongRequest.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import qs from 'qs';
+
 //env 설정해줘야힘.
 const apiUrl = process.env.VROONG_API_URL;
 
